2666503b3f133daf7cb35501eda054b0df90b262
blackmist87 <mike.blackmist@aemail4u.com>
2014-10-27 09:21:24 -0400

    Revert "First commit for master"
    This reverts commit 08929dab8950bce5d89916fd8a6bcf732b76731f.

--- app/models/journal.rb
+++
@@ -1,9 +0,0 @@
-class Journal < ActiveRecord::Base
-  belongs_to :user
-
-  attr_accessible :name, :blog_contents 
-
-  validates :name, presence: true, length: { in: 3..254 }
-  validates :blog_contents, presence: true, length: { in: 3..254 }
-  
-end
